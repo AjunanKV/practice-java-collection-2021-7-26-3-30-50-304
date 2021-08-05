092c5db2b32c1f376e37dc48e454ab3060916f8e
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-05 21:25:42 +0800

    [MarsRover][Kevin]update: remove complex command and make execute command can handle instructions with multiple inputs

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -10,14 +10,25 @@ public class StreamReduce {
     }
 
     public int getLastOdd(List<Integer> numbers) {
-        throw new NotImplementedException();
+        return numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .reduce(0, (first,second) -> second);
+
+        //throw new NotImplementedException();
     }
 
     public String getLongest(List<String> words) {
-        throw new NotImplementedException();
+        return words.stream()
+                .reduce("", (firstWord,secondWord) -> firstWord.length() > secondWord.length() ? firstWord : secondWord);
+
+        // throw new NotImplementedException();
     }
 
     public int getTotalLength(List<String> words) {
-        throw new NotImplementedException();
+        return words.stream()
+                .reduce((firstWord,secondWord) -> firstWord+secondWord)
+                .get().length();
+      //  throw new NotImplementedException();
     }
+
 }
